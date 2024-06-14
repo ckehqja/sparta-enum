befe93cb4d8eb6246b966cf7d155ec01a2208762
dobam <ckehqja@gmail.com>
2024-06-14 10:21:06 +0900

    쿠폰과 상품에 이넘 람다 추가

--- src/sparta/service/Coupon.java
+++ src/sparta/service/Coupon.java
@@ -12,17 +12,6 @@ public class Coupon {
     }
 
     public int calcPrice(DiscountEvent event) {
-        int benefit;
-
-        switch (event) {
-            case NONE -> benefit = couponPrice;
-            case SUMMER -> benefit = (int) (couponPrice * 0.1);
-            case WINTER -> benefit = (int) (couponPrice * 0.2);
-            case BLACK_FRIDAY -> benefit = (int) (couponPrice * 0.3);
-            case NEW_YEAR -> benefit = (int) (couponPrice * 0.5);
-            default -> benefit = 0;
-        }
-
-        return couponPrice + benefit;
+        return couponPrice + event.calc(couponPrice);
     }
 }
--- src/sparta/service/Product.java
+++ src/sparta/service/Product.java
@@ -12,17 +12,6 @@ public class Product {
     }
 
     public int calcPrice(DiscountEvent event) {
-        int discount;
-
-        switch (event) {
-            case NONE -> discount = price;
-            case SUMMER -> discount = (int) (price * 0.1);
-            case WINTER -> discount = (int) (price * 0.2);
-            case BLACK_FRIDAY -> discount = (int) (price * 0.3);
-            case NEW_YEAR -> discount = (int) (price * 0.5);
-            default -> discount = 0;
-        }
-
-        return price - discount;
+        return price - event.calc(price);
     }
 }
